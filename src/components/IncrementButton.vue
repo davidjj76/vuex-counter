ac2ebb7e44763cfdd57a1a3f000f13ca95f23a42
David Jiménez <djimenez@ets.es>
2017-04-11 15:37:58 +0200

    Initial commit

---
+++ src/components/IncrementButton.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+  <button @click.prevent="activate">+1</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    activate() {
+      console.log('+1 presed');
+    },
+  },
+};
+</script>
+
+<style lang="css">
+</style>
